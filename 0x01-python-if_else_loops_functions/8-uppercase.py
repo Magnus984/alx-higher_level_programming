a080ad66cea6c3cd9115345dc18e170e09938f4f
Magnus <nharnharyhaw984@gmail.com>
2024-01-03 17:18:35 +0000

    Fix: first requirement error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 def uppercase(str):
-    mod_str = ''
     for character in str:
         if ord(character) in range(ord('a'), ord('z') + 1):
-            mod_str += chr(ord(character) - 32)
-        else:
-            mod_str += character
-    print(mod_str)
+            character = chr(ord(character) - 32)
+        print("{}".format(character), end='')
+    print()
