99e7a8cb9979293e3e77c06c1bcdc0bd7eb70ab9
Magnus <nharnharyhaw984@gmail.com>
2024-01-03 17:10:00 +0000

    Fix: changed logic for checking if upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,17 +1,8 @@
-#!/usr/bin/python3
-def islower(c):
-    for i in range(ord('a'), ord('z') + 1):
-        if ord(c) == i:
-            return True
-        else:
-            continue
-        return False
-
-
+#!/usr/bin/python
 def uppercase(str):
     mod_str = ''
     for character in str:
-        if islower(character):
+        if ord(character) in range(ord('a'), ord('z') + 1):
             mod_str += chr(ord(character) - 32)
         else:
             mod_str += character
