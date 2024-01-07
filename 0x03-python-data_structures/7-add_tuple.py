53cd6b1138749235b5ad402471ee4cb715c8bb88
Magnus <nharnharyhaw984@gmail.com>
2024-01-07 22:49:38 +0000

    Fix: changed entire logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        tuple_res = (0, 0)
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        result_zero_index = tuple_a[0] + tuple_b[0]
-        result_one_index = 0
-        tuple_res = (result_zero_index, result_one_index)
-    elif len(tuple_a) == 0:
-        result_zero_index = tuple_b[0]
-        result_one_index = tuple_b[0]
-        tuple_res = (result_zero_index, result_one_index)
-    elif len(tuple_a) == 1:
-        result_zero_index = tuple_a[0] + tuple_b[0]
-        result_one_index = 0 + tuple_b[1]
-        tuple_res = (result_zero_index, result_one_index)
-    elif len(tuple_b) == 0:
-        result_zero_index = tuple_a[0]
-        result_one_index = tuple_a[0]
-        tuple_res = (result_zero_index, result_one_index)
-    elif len(tuple_b) == 1:
-        result_zero_index = tuple_a[0] + tuple_b[0]
-        result_one_index = tuple_a[0]
-        tuple_res = (result_zero_index, result_one_index)
-    else:
-        result_zero_index = tuple_a[0] + tuple_b[0]
-        result_one_index = tuple_a[1] + tuple_b[1]
-        tuple_res = (result_zero_index, result_one_index)
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+    result_zero_index = tuple_a[0] + tuple_b[0]
+    result_one_index = tuple_a[1] + tuple_b[1]
+    tuple_res = (result_zero_index, result_one_index)
     return tuple_res
