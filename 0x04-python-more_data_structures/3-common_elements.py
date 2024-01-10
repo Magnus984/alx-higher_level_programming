9d83e8b88f0f098e1797b2a01beec28bd54c8776
Magnus <nharnharyhaw984@gmail.com>
2024-01-10 21:01:31 +0000

    Fix: Put return value outside of loop

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    common_set = set()
     for element1 in set_1:
         for element2 in set_2:
             if element1 == element2:
-                return {element1, element2}
+                common_set.add(element1)
+    return common_set
