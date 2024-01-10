e7360284c10bbcb9d2dc3659ffbb02dcb99de88b
Magnus <nharnharyhaw984@gmail.com>
2024-01-10 20:53:14 +0000

    Fix: Output code error

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,4 +3,4 @@ def common_elements(set_1, set_2):
     for element1 in set_1:
         for element2 in set_2:
             if element1 == element2:
-                return element1
+                return {element1, element2}
