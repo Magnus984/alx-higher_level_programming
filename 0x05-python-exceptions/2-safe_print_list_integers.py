2c76fa303c90c2048f34ce726bbbbe9a6479915c
Magnus <nharnharyhaw984@gmail.com>
2024-01-22 20:35:58 +0000

    Fix: removed semicolon from a line

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            numElement += 1;
+            numElement += 1
         except (ValueError, TypeError):
             pass
     print()
