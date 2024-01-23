45caf033c1f794eae492c1b20d2861a907b6c17a
Magnus <nharnharyhaw984@gmail.com>
2024-01-23 22:09:43 +0000

    Fix: requirement error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Define a square class"""
 class Square:
-    """An empty class that defines a square.
-
-    This class doesn't have any methods or attributes yet."""
+    """An empty class that defines a square"""
     pass
