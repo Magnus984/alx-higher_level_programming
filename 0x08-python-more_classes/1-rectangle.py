bfc1921a038040b85bb20d9afd81a9f5bc63849a
Magnus <nharnharyhaw984@gmail.com>
2024-01-29 22:14:16 +0000

    Add: 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Defines a class rectangle
+Defines a class rectangle.
 """
 
 
@@ -14,8 +14,8 @@ class Rectangle:
             width(int): width of rectangle.
             height(int): height of rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
