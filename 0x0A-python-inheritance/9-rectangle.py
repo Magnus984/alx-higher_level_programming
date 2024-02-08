c542e360f3865ce04c4ccd7aa0f5cddfd32e5838
Magnus Tetteh <143129932+Magnus984@users.noreply.github.com>
2024-02-08 20:26:26 +0000

    Create 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Implements Rectangle class"""
+
+    def __init__(self, width, height):
+        """Initializes class rectangle.
+
+        Args:
+            width(int): width of rectangle.
+            height(int): height of rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
