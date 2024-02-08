af076a2ec4cb6dcaae72218975ecb7a85801a567
Magnus <nharnharyhaw984@gmail.com>
2024-02-08 22:46:21 +0000

    Realized since they were inheriting there wasn't a need to implement its own functions

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,7 +17,3 @@ class Square(Rectangle):
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Computes area of square"""
-        return self.__size * self.__size
