74271be3d6045f86d611c6c7cf6ff2d22e6cb720
Magnus <nharnharyhaw984@gmail.com>
2024-04-27 02:43:13 +0000

    Fixed the error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ import requests
 if __name__ == "__main__":
     req = requests.get('https://alx-intranet.hbtn.io/status')
     body = req.text
-    print("Body responses:")
+    print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
